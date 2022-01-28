3fea21549e8c9863501a6b3e68359b933f583db2
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-28 07:43:42 -0300

    chore: update vite pwa config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { VitePWA } from 'vite-plugin-pwa';
 export default defineConfig({
   plugins: [
     VitePWA({
+      includeAssets: [
+        '/*.svg',
+        'apple-touch-icon.png',
+        'favicon.ico',
+        'manifest.json'
+      ],
       devOptions: { enabled: true }
     })
   ]
