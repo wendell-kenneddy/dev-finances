32648c3a8280f05fb934cc48c79b454ba867b509
Wendell Kenneddy <wkenneddy505@gmail.com>
2022-01-28 07:30:09 -0300

    chore: add vite config

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    VitePWA({
+      devOptions: { enabled: true }
+    })
+  ]
+});
